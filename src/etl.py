913269f458fbf8664ebfa4ec1d86bb31a8e7f96e
Akshay Iyer <akshay.rajagopal@gmail.com>
2019-09-02 13:15:49 +0100

    Added code to load additional fact tables

--- src/etl.py
+++ src/etl.py
@@ -32,10 +32,10 @@ def process_artist_data(spark, source, dest):
         - source: Source directory/path for file
         - dest: Destination directory/path for file
     '''
-    file = 'name.basics.tsv.gz'
+    names_source = os.path.join(source, 'name.basics.tsv.gz')
 
     names_df_raw = spark.read.load(
-        source + file,
+        names_source,
         format="csv",
         sep="\t",
         inferSchema="true",
@@ -86,13 +86,13 @@ def process_artist_data(spark, source, dest):
 
     # Write artist dataframes to delta tables
     artists_df.write.format('delta').partitionBy("birthYear").mode('overwrite').save(
-        dest + "artists")
+        os.path.join(dest, "artists"))
 
     artists_prmry_prfsn_df.write.format("delta").mode('overwrite').save(
-        dest + "artists_prmry_profession")
+        os.path.join(dest, "artists_prmry_profession"))
 
     artists_knwn_fr_ttls_df.write.format("delta").mode('overwrite').save(
-        dest + "artists_knwnfor_titles")
+        os.path.join(dest, "artists_knwnfor_titles"))
 
 
 def process_title_data(spark, source, dest):
@@ -103,10 +103,10 @@ def process_title_data(spark, source, dest):
         - source: Source directory/path for file
         - dest: Destination directory/path for file
     '''
-    file = 'title.basics.tsv.gz'
+    titles_source = os.path.join(source, 'title.basics.tsv.gz')
 
     title_basics_df_raw = spark.read.load(
-        source + file,
+        titles_source,
         format="csv",
         sep="\t",
         inferSchema="true",
@@ -130,10 +130,10 @@ def process_title_data(spark, source, dest):
 
     # Save processed dataframes to parquet
     titles_df.write.format("delta").mode('overwrite').partitionBy(
-        "startYear").save(dest + "titles")
+        "startYear").save(os.path.join(dest, "titles"))
 
     titles_genres_df.write.format("delta").mode('overwrite').save(
-        dest + "titles_genres")
+        os.path.join(dest, "titles_genres"))
 
 
 def process_ratings_data(spark, source, dest):
@@ -144,10 +144,10 @@ def process_ratings_data(spark, source, dest):
         - source: Source directory/path for file
         - dest: Destination directory/path for file
     '''
-    file = 'title.ratings.tsv.gz'
+    title_ratings_source = os.path.join(source, 'title.ratings.tsv.gz')
 
     title_ratings_df_raw = spark.read.load(
-        source + file,
+        title_ratings_source,
         format="csv",
         sep="\t",
         inferSchema="true",
@@ -164,7 +164,59 @@ def process_ratings_data(spark, source, dest):
 
     # Save processed dataframes to parquet
     title_ratings_df.write.format("delta").mode('overwrite').save(
-        dest + "title_ratings")
+        os.path.join(dest, "title_ratings"))
+
+
+def process_top_movies(spark, source, dest):
+    '''
+    Create dataset with top 5 movies (TV and Cinema) by year
+    Params
+        - spark: Active spark session object
+        - source: Source directory/path for file
+        - dest: Destination directory/path for file
+    '''
+    titles_source = os.path.join(source, 'titles')
+    title_ratings_source = os.path.join(source, 'title_ratings')
+
+    # Import delta tables into dataframes
+    title_df = spark.read.format("delta").load(titles_source)
+    title_ratings_df = spark.read.format("delta").load(title_ratings_source)
+
+    # Create SQL views from dataframes
+    title_df.createOrReplaceTempView("titles")
+    title_ratings_df.createOrReplaceTempView("title_ratings")
+
+    # Create top 5 movies by title type by year dataframe
+    top_5_movies_by_year_df = spark.sql('''
+        select
+            titleId,
+            titleType,
+            startYear,
+            primaryTitle,
+            averageRating,
+            numVotes,
+            rank
+        from (
+            select
+                t.titleId,
+                t.titleType,
+                t.startYear,
+                t.primaryTitle,
+                tr.averageRating,
+                tr.numVotes,
+                rank() over (partition by t.titleType, t.startYear order by averageRating desc) as rank
+            from titles t
+            inner join title_ratings tr
+                on t.titleId = tr.titleId
+            where t.titleType in ('movie', 'tvMovie')) tmp
+        where rank < 6
+        ''')
+
+    # Save processed dataframes
+    top_5_movies_dest = os.path.join(dest, 'top_5_movies_by_year')
+
+    top_5_movies_by_year_df.write.format("delta").mode('overwrite').partitionBy(
+        "startYear").save(top_5_movies_dest)
 
 
 def main():
@@ -182,16 +234,14 @@ def main():
 
     base_path = os.path.join(os.path.abspath(''), os.pardir)
 
-    save_filepath = 'file://' + \
-        os.path.abspath(os.path.join(base_path, 'data', 'delta'))
-    temp_filepath = 'file://' + \
-        os.path.abspath(os.path.join(base_path, 'data', 'tmp'))
+    save_filepath = os.path.abspath(os.path.join(base_path, 'data', 'delta'))
+    temp_filepath = os.path.abspath(os.path.join(base_path, 'data', 'tmp'))
 
     # Process all dimension files
     process_artist_data(spark, temp_filepath, save_filepath)
     process_title_data(spark, temp_filepath, save_filepath)
     process_ratings_data(spark, temp_filepath, save_filepath)
-
+    process_top_movies(spark, save_filepath, save_filepath)
 
 if __name__ == "__main__":
     main()
